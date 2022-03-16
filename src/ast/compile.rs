d1b47005b91feb01d8df6f2637466ac3d7911193
nielinjie <nielinjie@gmail.com>
2022-03-16 16:06:33 +0800

    phase1 iter chain

--- src/ast/compile.rs
+++ src/ast/compile.rs
@@ -1,3 +1,5 @@
+use std::iter::{empty, once};
+
 use parity_wasm::elements::Instruction;
 
 use super::{Expr, Operator};
@@ -11,13 +13,22 @@ impl Compile for Expr {
         match self {
             Expr::Int(i) => vec![Instruction::I32Const(*i)],
             Expr::BinaryExpr { op, left, right } => {
-                let ops = &mut left.to_instructions();
-                ops.append(&mut right.to_instructions());
-                ops.push(match op {
-                    &Operator::Plus => Instruction::I32Add,
-                    &Operator::Minus => Instruction::I32Sub,
-                });
-                ops.to_owned()
+                empty()
+                    .chain(left.to_instructions())
+                    .chain(right.to_instructions())
+                    .chain(once(match op {
+                        &Operator::Plus => Instruction::I32Add,
+                        &Operator::Minus => Instruction::I32Sub,
+                    }))
+                    .collect()
+
+                // let ops = &mut left.to_instructions();
+                // ops.append(&mut right.to_instructions());
+                // ops.push(match op {
+                //     &Operator::Plus => Instruction::I32Add,
+                //     &Operator::Minus => Instruction::I32Sub,
+                // });
+                // ops.to_owned()
             }
             _ => unimplemented!(),
         }
