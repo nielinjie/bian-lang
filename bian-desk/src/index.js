25492f75fe311bcd4040a64bc4c9440358717b86
nielinjie <nielinjie@gmail.com>
2022-04-01 22:22:03 +0800

    doc2.8

--- bian-desk/src/index.js
+++ bian-desk/src/index.js
@@ -1,3 +1,10 @@
 import "react-reflex/styles.css";
 import "./index.css";
 import "./index.jsx";
+
+
+import { appWindow } from "@tauri-apps/api/window";
+appWindow.listen("tauri://close-requested", ({ event, payload }) => {
+  appWindow.close();
+  process.exit(0);
+});
